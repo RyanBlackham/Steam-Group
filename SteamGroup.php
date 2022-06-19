c221bbfe3e0772d6aef40f9dab4373b60b009561
Wiggyy <40574343+Wiggyy@users.noreply.github.com>
2022-06-19 23:24:30 +0100

    Update SteamGroup.php

--- SteamGroup.php
+++ SteamGroup.php
@@ -6,10 +6,10 @@ $steamID64 = 76561198448431038;
 function checkID($group, $ID) {
     foreach ($group->members->steamID64 as $currentID) {
         if ($ID == $currentID) {
-           return json_encode(['isInSteamGroup' => true]);
+           return json_encode(['inGroup' => true]);
         } 
     }  
-    return json_encode(['isInSteamGroup' => false]);
+    return json_encode(['inGroup' => false]);
 }
 
 echo checkID($steamXML, $steamID64);
